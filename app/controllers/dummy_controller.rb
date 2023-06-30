90d3ae3123309cf6314ff67de5e751d3d700b63e
Nishi Kant Sharma <nishiit24@gmail.com>
2023-06-30 08:08:49 +0530

    test rubocop

--- app/controllers/dummy_controller.rb
+++ app/controllers/dummy_controller.rb
@@ -7,15 +7,14 @@ class DummyController < ApplicationController
   def create
     @message = params[:message]
     if @message.blank?
-      render json: { error: "Message cannot be blank" }, status: :unprocessable_entity
+      render json: { error: 'Message cannot be blank' }, status: :unprocessable_entity
     else
-             render json: { success: true, message: @message }, status: :created
+      render json: { success: true, message: @message }, status: :created
     end
   end
 
   def show
-    @message = "Hello, RuboCop!"
-          render json: { message: @message }
+    @message = 'Hello, RuboCop!'
+    render json: { message: @message }
   end
-
 end
